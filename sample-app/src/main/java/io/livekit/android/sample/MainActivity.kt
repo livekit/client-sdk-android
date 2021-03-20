e7407c3e06cdf8fb287b3ff9cdfaa2b66508e599
David Liu <davidliu@deviange.net>
2021-03-20 12:02:27 +0900

    request permissions needed for audio/video capture

--- sample-app/src/main/java/io/livekit/android/sample/MainActivity.kt
+++ sample-app/src/main/java/io/livekit/android/sample/MainActivity.kt
@@ -1,10 +1,16 @@
 package io.livekit.android.sample
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import io.livekit.android.sample.databinding.MainActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +36,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+
+        requestPermissions()
+
+    }
+
+    private fun requestPermissions() {
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { grants ->
+                for (grant in grants.entries) {
+                    if (!grant.value) {
+                        Toast.makeText(
+                            this,
+                            "Missing permission: ${grant.key}",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+            }
+        val neededPermissions = listOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA)
+            .filter {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    it
+                ) == PackageManager.PERMISSION_DENIED
+            }
+            .toTypedArray()
+        if (neededPermissions.isNotEmpty()) {
+            requestPermissionLauncher.launch(neededPermissions)
+        }
     }
 
     companion object {
