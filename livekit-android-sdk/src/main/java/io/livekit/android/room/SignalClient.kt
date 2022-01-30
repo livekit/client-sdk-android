769a23d3131f082784effaef70964f21ad36f2dc
davidliu <davidliu@deviange.net>
2022-01-30 22:34:28 +0900

    v0.8.1 (#50)
    * downgrade protocol version for release
    
    * v0.8.1

--- livekit-android-sdk/src/main/java/io/livekit/android/room/SignalClient.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/room/SignalClient.kt
@@ -560,7 +560,7 @@ constructor(
         const val SD_TYPE_ANSWER = "answer"
         const val SD_TYPE_OFFER = "offer"
         const val SD_TYPE_PRANSWER = "pranswer"
-        const val PROTOCOL_VERSION = 6
+        const val PROTOCOL_VERSION = 5
         const val SDK_TYPE = "android"
 
         private fun iceServer(url: String) =
