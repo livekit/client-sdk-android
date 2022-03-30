ce23d3cfcc3d30dfba05d1722317243694606714
davidliu <davidliu@deviange.net>
2022-03-30 23:43:33 +0900

    Directly test if VP8/H264 is available for device

---
+++ video-encode-decode-test/src/androidTest/java/io/livekit/android/videoencodedecode/test/OfficialCodecSupportTest.kt
@@ -0,0 +1,32 @@
+package io.livekit.android.videoencodedecode.test
+
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.webrtc.DefaultVideoEncoderFactory
+import org.webrtc.EglBase
+import org.webrtc.PeerConnectionFactory
+
+class OfficialCodecSupportTest {
+    val defaultVideoEncoderFactory = DefaultVideoEncoderFactory(EglBase.create().eglBaseContext, true, true)
+
+    @Before
+    fun setup() {
+        PeerConnectionFactory.initialize(
+            PeerConnectionFactory.InitializationOptions
+                .builder(InstrumentationRegistry.getInstrumentation().targetContext)
+                .createInitializationOptions()
+        )
+    }
+
+    @Test
+    fun isVP8Supported() {
+        Assert.assertTrue(defaultVideoEncoderFactory.supportedCodecs.any { it.name == "VP8" })
+    }
+
+    @Test
+    fun isH264Supported() {
+        Assert.assertTrue(defaultVideoEncoderFactory.supportedCodecs.any { it.name == "H264" })
+    }
+}
