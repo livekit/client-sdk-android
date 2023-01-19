b6ff176c9b14e947ab38a5fe6c5f48132ea657d8
davidliu <davidliu@deviange.net>
2023-01-19 21:10:59 +0900

    Foreground service example to keep call running in background (#175)

---
+++ sample-app-common/src/main/java/io/livekit/android/sample/service/ForegroundService.kt
@@ -0,0 +1,54 @@
+package io.livekit.android.sample.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+
+/**
+ * A foreground service is required for screen capture on API level Q (29) and up.
+ * This a simple default foreground service to display a notification while screen
+ * capturing.
+ */
+
+open class ForegroundService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+
+        val actualNotification =
+            NotificationCompat.Builder(this, DEFAULT_CHANNEL_ID)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+
+        startForeground(DEFAULT_NOTIFICATION_ID, actualNotification)
+        return START_NOT_STICKY
+    }
+    
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            DEFAULT_CHANNEL_ID,
+            "Foreground",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null // not used.
+    }
+
+    companion object {
+        const val DEFAULT_NOTIFICATION_ID = 3456
+        const val DEFAULT_CHANNEL_ID = "livekit_example_foreground"
+    }
+}
