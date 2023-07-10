89b9501075fedef818a7eef1a967f7097c18f160
CloudWebRTC <duanweiwei1982@gmail.com>
2023-07-11 07:37:45 +0800

    Update livekit-android-sdk/src/main/java/io/livekit/android/room/track/RemoteAudioTrack.kt
    Co-authored-by: davidliu <davidliu@deviange.net>

--- livekit-android-sdk/src/main/java/io/livekit/android/room/track/RemoteAudioTrack.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/room/track/RemoteAudioTrack.kt
@@ -5,7 +5,7 @@ import org.webrtc.RtpReceiver
 import org.webrtc.RtpSender
 import org.webrtc.RtpTransceiver
 
-class RemoteAudioTrack(name: String, rtcTrack: AudioTrack, receiver: RtpReceiver) : io.livekit.android.room.track.AudioTrack(name, rtcTrack)
+class RemoteAudioTrack(name: String, rtcTrack: AudioTrack, internal val receiver: RtpReceiver) : io.livekit.android.room.track.AudioTrack(name, rtcTrack)
 {
     internal var receiver: RtpReceiver
     init {
