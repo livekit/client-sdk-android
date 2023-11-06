35b443db073650c481606c49f4c921f074d09e93
Serhiy Novoseletskiy <novoseletskiyserhiy@gmail.com>
2023-11-06 18:22:14 +0200

    Prevent initializing video renderer if room is disconnected

--- livekit-android-sdk/src/main/java/io/livekit/android/room/Room.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/room/Room.kt
@@ -998,6 +998,9 @@ constructor(
      * // TODO(@dl): can this be moved out of Room/SDK?
      */
     fun initVideoRenderer(viewRenderer: SurfaceViewRenderer) {
+        if (state == State.DISCONNECTED) {
+            return
+        }
         viewRenderer.init(eglBase.eglBaseContext, null)
         viewRenderer.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
         viewRenderer.setEnableHardwareScaler(false)
@@ -1007,6 +1010,9 @@ constructor(
      * // TODO(@dl): can this be moved out of Room/SDK?
      */
     fun initVideoRenderer(viewRenderer: TextureViewRenderer) {
+        if (state == State.DISCONNECTED) {
+            return
+        }
         viewRenderer.init(eglBase.eglBaseContext, null)
         viewRenderer.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
         viewRenderer.setEnableHardwareScaler(false)
