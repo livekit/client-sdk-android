7fccdb86574ae6093f6a7231e2067eeca95c316c
Seongeun Jin <seongeun.jin@nearthlab.com>
2024-04-02 13:41:26 +0900

    Set access modifier

--- sample-app-compose/src/main/java/io/livekit/android/composesample/MainActivity.kt
+++ sample-app-compose/src/main/java/io/livekit/android/composesample/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : ComponentActivity() {
             LKLog.i { "network connection available, reconnecting" }
         }
     }
-    val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
