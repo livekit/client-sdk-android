937932087550c40b16bed07ac55544b75f61be4b
davidliu <davidliu@deviange.net>
2024-07-01 21:47:04 +0900

    Make setter for LocalTrackPublication.muted public (#447)
    * Make setter for LocalTrackPublication.muted public
    
    * spotless

--- livekit-android-sdk/src/main/java/io/livekit/android/room/track/LocalTrackPublication.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/room/track/LocalTrackPublication.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 LiveKit, Inc.
+ * Copyright 2023-2024 LiveKit, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ class LocalTrackPublication(
      */
     override var muted: Boolean
         get() = super.muted
-        set(muted) {
+        public set(muted) {
             if (muted == this.muted) {
                 return
             }
