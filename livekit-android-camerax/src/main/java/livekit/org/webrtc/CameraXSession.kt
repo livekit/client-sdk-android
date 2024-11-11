4ddcc6efa1d3f8b41eefa7170899af3c87e9e174
kasem <kasem.jaffer@gmail.com>
2024-11-10 18:41:50 -0800

    Add use cases to createCameraProvider

--- livekit-android-camerax/src/main/java/livekit/org/webrtc/CameraXSession.kt
+++ livekit-android-camerax/src/main/java/livekit/org/webrtc/CameraXSession.kt
@@ -35,6 +35,7 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.core.Preview.SurfaceProvider
+import androidx.camera.core.UseCase
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
@@ -57,6 +58,7 @@ internal constructor(
     private val width: Int,
     private val height: Int,
     private val frameRate: Int,
+    private val useCases: Array<out UseCase> = emptyArray(),
 ) : CameraSession {
 
     private var state = SessionState.RUNNING
@@ -171,7 +173,13 @@ internal constructor(
                         cameraProvider.unbindAll()
 
                         // Bind use cases to camera
-                        camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, imageAnalysis, preview)
+                        camera = cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            imageAnalysis,
+                            preview,
+                            *useCases,
+                        )
 
                         cameraThreadHandler.post {
                             sessionCallback.onDone(this@CameraXSession)
