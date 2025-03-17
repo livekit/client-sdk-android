e23dd931ec74602f58e004acec9a6472f50d6117
davidliu <davidliu@deviange.net>
2025-03-18 02:28:18 +0900

    spotless and changeset

--- livekit-android-test/src/main/java/io/livekit/android/test/mock/dagger/TestRTCModule.kt
+++ livekit-android-test/src/main/java/io/livekit/android/test/mock/dagger/TestRTCModule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 LiveKit, Inc.
+ * Copyright 2023-2025 LiveKit, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,6 +80,7 @@ object TestRTCModule {
     fun audioPrewarmer(): AudioRecordPrewarmer {
         return NoAudioRecordPrewarmer()
     }
+
     @Provides
     @Singleton
     fun peerConnectionFactory(
