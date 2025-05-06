071c0622a6dff7bdc47814f67940d556153d1a36
davidliu <davidliu@deviange.net>
2025-05-06 17:31:56 +0900

    expose specific floats arguments for coordinate matrixes

--- examples/selfie-segmentation/src/main/java/io/livekit/android/selfie/jsshader/Shader.kt
+++ examples/selfie-segmentation/src/main/java/io/livekit/android/selfie/jsshader/Shader.kt
@@ -1,23 +1,46 @@
 package io.livekit.android.selfie.jsshader
 
 import android.opengl.GLES20
+import java.nio.FloatBuffer
 
 object ShaderUtil {
-    fun loadCoordMatrix(inPosLocation: Int, inTcLocation: Int, texMatrixLocation: Int, texMatrix: FloatArray) {
+    fun loadCoordMatrix(
+        inPosLocation: Int,
+        inPosFloats: FloatBuffer = FULL_RECTANGLE_BUFFER,
+        inTcLocation: Int,
+        inTcFloats: FloatBuffer = FULL_RECTANGLE_TEXTURE_BUFFER,
+        texMatrixLocation: Int,
+        texMatrix: FloatArray,
+    ) {
         // Upload the vertex coordinates.
         GLES20.glEnableVertexAttribArray(inPosLocation);
-        GLES20.glVertexAttribPointer(inPosLocation, /* size= */ 2,
-            /* type= */ GLES20.GL_FLOAT, /* normalized= */ false, /* stride= */ 0,
-            FULL_RECTANGLE_BUFFER);
+        GLES20.glVertexAttribPointer(
+            inPosLocation,
+            /* size= */ 2,
+            /* type= */ GLES20.GL_FLOAT,
+            /* normalized= */ false,
+            /* stride= */ 0,
+            inPosFloats,
+        );
 
         // Upload the texture coordinates.
         GLES20.glEnableVertexAttribArray(inTcLocation);
-        GLES20.glVertexAttribPointer(inTcLocation, /* size= */ 2,
-            /* type= */ GLES20.GL_FLOAT, /* normalized= */ false, /* stride= */ 0,
-            FULL_RECTANGLE_TEXTURE_BUFFER);
+        GLES20.glVertexAttribPointer(
+            inTcLocation,
+            /* size= */ 2,
+            /* type= */ GLES20.GL_FLOAT,
+            /* normalized= */ false,
+            /* stride= */ 0,
+            inTcFloats,
+        );
 
         // Upload the texture transformation matrix.
         GLES20.glUniformMatrix4fv(
-            texMatrixLocation, 1 /* count= */, false /* transpose= */, texMatrix, 0 /* offset= */);
+            texMatrixLocation,
+            /* count= */ 1,
+            /* transpose= */ false,
+            texMatrix,
+            /* offset= */0,
+        );
     }
 }
