7e9f2dbd4690dd85db757f173845893289f4f747
davidliu <davidliu@deviange.net>
2026-05-05 03:10:15 +0900

    spotless

--- livekit-android-sdk/src/main/java/io/livekit/android/room/participant/RemoteParticipant.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/room/participant/RemoteParticipant.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 LiveKit, Inc.
+ * Copyright 2023-2026 LiveKit, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,7 +209,6 @@ class RemoteParticipant(
             @Suppress("SwallowedException")
             try {
                 track.stop()
-
             } catch (e: Exception) {
                 // track may already be disposed, ignore.
             }
