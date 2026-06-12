9ec9e7b45ea850c9452f49640dbfa8be079eb97c
davidliu <davidliu@deviange.net>
2026-06-12 18:32:39 +0900

    spotless

--- livekit-android-test/src/test/java/io/livekit/android/room/datastream/StreamReaderTest.kt
+++ livekit-android-test/src/test/java/io/livekit/android/room/datastream/StreamReaderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 LiveKit, Inc.
+ * Copyright 2025-2026 LiveKit, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package io.livekit.android.room.datastream
 
-import io.livekit.android.room.datastream.StreamException
 import io.livekit.android.room.datastream.incoming.ByteStreamReceiver
 import io.livekit.android.room.datastream.incoming.IncomingDataStreamManagerImpl
 import io.livekit.android.test.BaseTest
