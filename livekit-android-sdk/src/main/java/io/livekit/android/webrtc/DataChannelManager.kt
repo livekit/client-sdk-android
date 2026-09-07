8714e2a06ba04eedd92c7754afb4461c19db8b10
davidliu <davidliu@deviange.net>
2026-09-08 03:29:55 +0900

    clean up orphaned code

--- livekit-android-sdk/src/main/java/io/livekit/android/webrtc/DataChannelManager.kt
+++ livekit-android-sdk/src/main/java/io/livekit/android/webrtc/DataChannelManager.kt
@@ -24,7 +24,6 @@ import io.livekit.android.util.flowDelegate
 import io.livekit.android.webrtc.peerconnection.RTCThreadToken
 import io.livekit.android.webrtc.peerconnection.executeOnRTCThread
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.takeWhile
 import livekit.org.webrtc.DataChannel
@@ -57,16 +56,6 @@ class DataChannelManager(
             .cancelOnSignal(signal)
             .takeWhile { it > amount }
             .collect()
-    }
-
-    suspend fun waitUntilOpen() {
-        if (state == DataChannel.State.OPEN) {
-            return
-        }
-        val signal = ::disposed.flow.map { if (it) Unit else null }
-        ::state.flow
-            .cancelOnSignal(signal)
-            .first { it == DataChannel.State.OPEN }
     }
 
     override fun onBufferedAmountChange(previousAmount: Long) {
